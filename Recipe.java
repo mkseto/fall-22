f2ad9fec45c3686b8cfb589d5c16b9a8fe7b5bb8
Olivia Nolan Shafer <onolanshafer@sandiego.edu>
2022-11-17 17:45:22 -0800

    added compareTo in recipe.java

--- Recipe.java
+++ Recipe.java
@@ -4,11 +4,16 @@
  */
 
 /**
- *
+ * Data class representing a recipe in a cookbook
  * @author onolanshafer
  */
 public record Recipe(String name, String[] ingredients, int preptime, String[] steps) {
     public String getName() {
+        /* 
+         Returns name of a recipe
+         Params: none
+         Returns 
+         */
         return name;
     }
 
@@ -24,6 +29,20 @@ public record Recipe(String name, String[] ingredients, int preptime, String[] s
         return steps;
     }
     
+    public int compareTo(Object other){
+        /*
+        Compares Recipe objects based on prep time alone. 
+        Returns difference between prep times of two Recipe objects. 
+        */
+    
+        if(this.getClass() != other.getClass()){
+            throw new java.lang.RuntimeException("compareTo: incompatible objects");
+       }
+        if(this == other) return 0; 
+        Recipe otherRecipe = (Recipe)other;
+        return this.preptime() - otherRecipe.preptime();
+    } 
+
     public String toString(){
         return "";
     }
