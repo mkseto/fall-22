42b3f3548b8844fda483b424d23248f0058ec853
Olivia Nolan Shafer <onolanshafer@sandiego.edu>
2022-12-07 18:09:13 -0800

    read recipe error

--- ReadRecipeTest.java
+++ ReadRecipeTest.java
@@ -7,5 +7,5 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.*;
 
 public class ReadRecipeTest {
-    
+    Object obj = new JSONParser().parse(new FileReader("db-recipes.json"));
 }
